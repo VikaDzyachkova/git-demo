1fb71ac179b4d9ad7372187e5101f488c49c102e
Vika Dzyachkova <dzyachkovavika@gmail.com>
2022-02-01 20:21:09 +0300

    study stash

---
+++ les1.js
@@ -0,0 +1,21 @@
+// Example 1
+
+if (1 === 1) {
+    console.log("It's true ! // Example 1 ")
+}
+
+
+
+// Example 2
+
+let rating = 3;
+if (rating === 3) {
+    console.log('SUPER !!! // Example 2')
+}
+
+// Example 3
+
+let num =37;
+if (num % 2 != 0) {
+    console.log("ODD NUMBER!")
+}
