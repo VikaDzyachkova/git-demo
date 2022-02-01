ae17b7bcb85d0db26dc69c151f5d71d77f86349b
Vika Dzyachkova <dzyachkovavika@gmail.com>
2022-02-01 13:00:11 +0300

    added code

--- les3.js
+++ les3.js
@@ -171,3 +171,18 @@ if (password.length <= 6) {
 else {
     console.log("Password too short!")
 }
+
+
+let password2 = "house";
+
+if (password.length <= 6) {
+    if (password.indexOf(" ") !== -1) {
+        console.log(`Valid password. Password cannot includ space.`);
+        }
+    else {
+        console.log("Valid password. Password must be longer!");
+    }
+}
+else {
+    console.log("Password too short!")
+}
